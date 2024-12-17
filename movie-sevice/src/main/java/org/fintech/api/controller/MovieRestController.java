9d731a7905ddfad66183ef90366fdd94a8a352b4
Sergey T <moolycooly2004@gmail.com>
2024-12-17 19:15:53 +0300

    deleted * import

--- movie-sevice/src/main/java/org/fintech/api/controller/MovieRestController.java
+++ movie-sevice/src/main/java/org/fintech/api/controller/MovieRestController.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.fintech.api.ApiPaths;
 import org.fintech.api.model.CreateMovieRequest;
 import org.fintech.api.model.MovieDto;
@@ -15,14 +14,23 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+
 
 import java.time.LocalDate;
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@Slf4j
 @Tag(name = "Фильмы", description = "API для управления фильмами")
 public class MovieRestController {
 
